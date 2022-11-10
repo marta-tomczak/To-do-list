bc7199981958fc3b60aa0855015e354bf8bffbc6
Marta Tomczak <marta.beata.tomczak@gmail.com>
2022-11-10 16:34:33 +0100

    new path for og:image

--- script/main.js
+++ script/main.js
@@ -1,12 +1,11 @@
+const tasks = [];
 const form = document.querySelector(".js-form");
 const input = form.querySelector(".js-newTask");
 const formButton = form.querySelector(".js-formButton");
 const jsTasks = document.querySelector(".js-tasks");
 
 const createTaskElement = (props) => {
-  const element = document.createElement("div");
-
-  element.innerHTML = `
+  const html = `
     <li class="tasks__item">
         <button class="tasks__button ${
           props.done ? `tasks__button--${props.done}` : ""
@@ -15,19 +14,19 @@ const createTaskElement = (props) => {
         <button class="tasks__button tasks__button--delete js-task-delete">🗑️</button>
     </li>`;
 
-  const li = element.children[0];
-  const buttonType = li.querySelector(".js-task-type");
-  const buttonDelete = li.querySelector(".js-task-delete");
+  // const li = element.children[0];
+  // const buttonType = li.querySelector(".js-task-type");
+  // const buttonDelete = li.querySelector(".js-task-delete");
 
-  buttonDelete.addEventListener("click", () => {
-    li.remove();
-  });
+  // buttonDelete.addEventListener("click", () => {
+  //   li.remove();
+  // });
 
-  buttonType.addEventListener("click", () => {
-    buttonType.classList.toggle("tasks__button--done");
-  });
+  // buttonType.addEventListener("click", () => {
+  //   buttonType.classList.toggle("tasks__button--done");
+  // });
 
-  return li;
+  return html;
 };
 
 const clearInput = () => {
@@ -35,8 +34,29 @@ const clearInput = () => {
 };
 
 const addTask = () => {
-  jsTasks.append(createTaskElement({ done: undefined, text: input.value }));
+  const value = input.value.trim();
+
+  if (!value) return;
+
+  const task = {
+    done: undefined,
+    text: value,
+  };
+
+  tasks.push(task);
+
   clearInput();
+  render();
+};
+
+const clearTask = () => {
+  while (jsTasks.firstElementChild) jsTasks.firstElementChild.remove();
+};
+
+const render = () => {
+  clearTask();
+
+  tasks.forEach((task) => jsTasks.append(createTaskElement(task)));
 };
 
 form.addEventListener("submit", (event) => {
